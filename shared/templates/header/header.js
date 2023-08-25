6ca79abd3f514b9328934e9a7966e668d0d59ffb
MohamadAmin Baqeri <mhmdaminbaqeri@gmail.com>
2023-08-26 00:41:01 +0330

    add light theme, change theme when system theme is changed

--- shared/templates/header/header.js
+++ shared/templates/header/header.js
@@ -1,3 +1,20 @@
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) {
+    changeTheme(document.getElementById("system-theme-btn-lg"));
+}
+
+window.matchMedia('(prefers-color-scheme: light)')
+    .addEventListener('change', ({matches}) => {
+        let themeBtn = document.getElementById("system-theme-btn-lg");
+        if (themeBtn.classList.contains("theme-option-btn-active")) {
+            if (matches) {
+                changeTheme(themeBtn);
+            } else {
+                changeTheme(themeBtn);
+            }
+        }
+})
+
+
 function toggleVersions(element) {
     let versionsList = document.getElementById("version-list");
     if (element.getAttribute("aria-expanded") === 'true') {
@@ -130,12 +147,14 @@ function closeMenu(element) {
 }
 
 function changeTheme(element) {
-    // TODO: can change theme automatically by css because by using js when system color change the website theme isn't change
+    // TODO: change theme automatically by css because by using js when system color change the website theme isn't change
+    let themeSection = document.getElementById("theme-section-id");
     let themeState = document.getElementById("theme-state");
     let themeIcon = document.getElementById("theme-icon-btn");
     let changeBtnLg = document.getElementById("theme-icon-btn-lg");
     let themeBtnLg = document.getElementsByClassName("theme-option-btn-active")[0];
     let darkTheme = window.matchMedia('(prefers-color-scheme: dark)');
+    let body = document.body;
 
     if (element.innerText === "System") {
         themeState.innerText = element.innerText;
@@ -143,37 +162,57 @@ function changeTheme(element) {
             themeBtnLg.classList.remove("theme-option-btn-active");
             document.getElementById("system-theme-btn-lg").classList.add("theme-option-btn-active");
         }
+        // change to dark if system theme was dark and site was light
         if (darkTheme.matches && themeIcon.classList.contains("bi-brightness-high")) {
+            themeSection.classList.remove("theme-section-light");
+            themeSection.classList.add("theme-section-dark");
             themeIcon.classList.remove("bi-brightness-high");
             themeIcon.classList.add("bi-moon-stars");
             changeBtnLg.classList.remove("bi-brightness-high");
             changeBtnLg.classList.add("bi-moon-stars");
+            body.classList.remove("light");
+            body.classList.add("dark");
+        // change to light if system theme was light and site was dark
         } else if (!darkTheme.matches && themeIcon.classList.contains("bi-moon-stars")) {
-            themeIcon.classList.add("bi-brightness-high");
+            themeSection.classList.remove("theme-section-dark");
+            themeSection.classList.add("theme-section-light");
             themeIcon.classList.remove("bi-moon-stars");
-            changeBtnLg.classList.add("bi-brightness-high");
+            themeIcon.classList.add("bi-brightness-high");
             changeBtnLg.classList.remove("bi-moon-stars");
+            changeBtnLg.classList.add("bi-brightness-high");
+            body.classList.remove("dark");
+            body.classList.add("light");
         }
     } else if (themeState.innerText !== element.innerText) {
         themeState.innerText = element.innerText;
+        // change to dark
         if (element.innerText === "Dark") {
             if (themeBtnLg.innerText !== "Dark") {
                 themeBtnLg.classList.remove("theme-option-btn-active");
                 document.getElementById("dark-theme-btn-lg").classList.add("theme-option-btn-active");
             }
+            themeSection.classList.remove("theme-section-light");
+            themeSection.classList.add("theme-section-dark");
             themeIcon.classList.remove("bi-brightness-high");
             themeIcon.classList.add("bi-moon-stars");
             changeBtnLg.classList.remove("bi-brightness-high");
             changeBtnLg.classList.add("bi-moon-stars");
+            body.classList.remove("light");
+            body.classList.add("dark");
         } else {
+            // change to light
             if (themeBtnLg.innerText !== "Light") {
                 themeBtnLg.classList.remove("theme-option-btn-active");
                 document.getElementById("light-theme-btn-lg").classList.add("theme-option-btn-active");
             }
+            themeSection.classList.remove("theme-section-dark");
+            themeSection.classList.add("theme-section-light");
             themeIcon.classList.add("bi-brightness-high");
             themeIcon.classList.remove("bi-moon-stars");
             changeBtnLg.classList.add("bi-brightness-high");
             changeBtnLg.classList.remove("bi-moon-stars");
+            body.classList.remove("dark");
+            body.classList.add("light");
         }
     }
 
